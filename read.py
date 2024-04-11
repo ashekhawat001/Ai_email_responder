7a64eacbcdeb15895872ae20df7f0ebcb5c3280b
aditya <104897379+ashekhawat001@users.noreply.github.com>
2024-04-12 02:45:25 +0530

    responses are getting generated and sent automatically

--- read.py
+++ read.py
@@ -13,23 +13,30 @@ model = genai.GenerativeModel('gemini-pro')
 
 promt="""your role is to reply to customers of XYZ marketing agency. Write a professional and courteous email response to a customer inquiry about or requesting support. Write the email based on or as a reply to the text labeled 'input email' given below. In the email, be sure to:
 
-    NOT INCLUDE PLACE HOLDERS FOR DATA NOT AVAILBLE TO YOU, example [name]
+    
     Acknowledge the customer's concern and thank them for contacting you.
     Briefly explain the issue based on the the email received
     Offer a solution if possible, or outline the next steps that will be taken to resolve the issue.
     Personalize the closing by including the customer's name (if available).
     don't end the email with [your name] instead use the company's name
+
+    IMPORTANT -- NOT INCLUDE PLACE HOLDERS FOR DATA NOT AVAILBLE TO YOU, example [name], OR DATA THAT YOU INTEND TO FILL YOURSELF, ALWAYS GIVE PRESICSE RESPONSE DO'NT LEAVE ANYTHING UNFINISHED AS THESE ARE THE ACTUAL FINAL RESPONSE GOING OUT EXACTLY AS YPU WRITE THEM
+
+
     if body of email is empty then don't generate any response
+    Also keep in kind to give the generated text in proper formamting by using next line \ + n etc
 
     but keep in mind customers can inquire about other things like jobs posted by the agency or updates on ongoing work with the agency in those cases dont spin up facts just tell them to wait and that someone will resolve their query shortly
-
+    IF THE query by customer is irrelevant keep the reply to one line
 
     input email :"""
 
 gmail = Gmail()
 
+
+
 query_params={
-    "newer_than" : (2, "days")
+    "newer_than" : (1, "days")
 }
 
 messages = gmail.get_unread_messages(query=construct_query(query_params))
@@ -45,14 +52,33 @@ for message in messages:
         if message.plain:            
             store.write("body" + message.plain)
         else:
-            store.write("body: Empty")
+            store.write("body: ")
     count+=1
      
     with open('email_store.txt', 'r', encoding='utf-8') as store:
         email_content = store.read() 
 
     response = model.generate_content(str(promt+email_content))
+
+    msg_html = str(response.text)
+    msg_html = msg_html.replace("\n", "<br>")
+    
+    params = {
+        "to": "7ik4h.test@inbox.testmail.app",
+        "sender": message.recipient,
+        "subject": "re"+ message.subject,
+        "msg_html": msg_html,
+        
+    }
+
+    message = gmail.send_message(**params)  # equivalent to send_message(to="you@youremail.com", sender=...)
+    
     print(response.text)
+    print("Message sent successfully")
+
+
+
+    
 
 print("finished Found "+ str( count) +" messages")        
     
